091490a280da1079535fb826a2a3d379c56440c4
James Chu <james60708@gmail.com>
2020-06-01 19:08:26 +0800

    add order management related view & controller

---
+++ bin/main/static/js/ordermanagement.js
@@ -0,0 +1,91 @@
+(function($) {
+
+    'use strict';
+
+    $.getJSON('/GetAllOrder', function (data) {
+        console.log(data);
+        $("#ordertable tr").remove();
+        var template = `
+        <tr data-id="%(id)s">
+            <td scope="row">%(id)s</td>
+            <td>%(trip.title)s</td>
+            <td>%(trip.travelCode)s</td>
+            <td>%(trip.startDate)s</td>
+            <td>%(trip.endDate)s</td>
+            <td>%(quantity)s</td>
+            <td>%(totalPrice)s</td>
+        </tr>
+        `;
+        var i;
+        for(i = 0;i < data.length; ++i){
+            var tmp = sprintf(template, data[i]);
+            //console.log(tmp);
+            $("#ordertable").append(tmp);
+        }
+        
+        $("#wholeordertable").Tabledit({
+            url: '/editOrder',
+            deleteButton: true,
+            editButton: true,
+            restoreButton: false,
+            buttons: {
+                delete: {
+                    class: 'btn btn-sm btn-danger',
+                    html: 'DELETE',
+                    action: 'delete'
+                },
+                confirm: {
+                    class: 'btn btn-sm btn-default',
+                    html: 'Are you sure?'
+                },
+                edit: {
+                    class: 'btn btn-sm btn-success',
+                    html: 'EDIT',
+                    action: 'edit'
+                },
+            },
+            
+            columns: {
+            	
+              identifier: [0, "id"],
+              editable: [
+            	  [5, 'quantity']
+              ]
+            },
+           onSuccess: function(data, textStatus, jqXHR) {
+                console.log('onSuccess(data, textStatus, jqXHR)');
+                console.log(data);
+                alert(data["msg"]);
+                console.log(textStatus);
+                console.log(jqXHR);
+                location.reload();
+            },
+            onFail: function(jqXHR, textStatus, errorThrown) {
+                console.log('onFail(jqXHR, textStatus, errorThrown)');
+                console.log(jqXHR);
+                console.log(textStatus);
+                console.log(errorThrown);
+                alert(errorThrown);
+                location.reload();
+            },
+            onAlways: function() {
+                console.log('onAlways()');
+            },
+            onAjax: function(action, serialize) {
+                console.log('onAjax(action, serialize)');
+                console.log(action);
+                console.log(serialize);
+            }
+          });
+    });
+    
+
+    $(function() {
+      
+    });
+
+})(jQuery);
+
+
+
+
--- src/main/java/travel/booking/controller/CustomerOrderController.java
+++ src/main/java/travel/booking/controller/CustomerOrderController.java
@@ -14,6 +14,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.google.gson.annotations.SerializedName;
+
 import travel.booking.Global;
 import travel.booking.container.*;
 
@@ -26,148 +28,126 @@ import javax.validation.Valid;
 public class CustomerOrderController {         
 	@Resource(name = "loginInfoSession")
 	LoginInfo loginInfo;
+	String errorMessage = "Edit failed!";
+	
+	private class DetailedOrder{
+		public String id;
+		public Trip trip;
+		public int quantity;
+		public int totalPrice;
+	}
 	
 	@RequestMapping(value={"ordermanagement", "ordermanagement.html"})
     public String getCustomerOrderPage(Model model) {
-    	System.out.println("CustomerOrderPage");
-    	model.addAttribute("loginInfo", loginInfo);
-        return "ordermanagement";                        
+		model.addAttribute("loginInfo", loginInfo);
+		if(loginInfo.islogin) {
+			System.out.println("CustomerOrderPage");
+	        return "ordermanagement";  
+		}
+		else
+			return "index";                    
     }
-	/*
+	
 	@RequestMapping(path = "/GetAllOrder", produces = "application/json; charset=UTF-8")
     @ResponseBody
-    public List<Order> getCustomerOrder(){
+    public List<DetailedOrder> getCustomerOrder(){
 		System.out.println("GetAllOrder");
-    	List<Order> Orderlist=Global.db.getOrder(loginInfo.account.id);
-        for(Order order: Orderlist){
-            order.room.price *= order.quantity; 
-        }
-    	//System.out.println(Orderlist);
+    	List<Order> Orderlist = Global.db.getOrder(loginInfo.account.id);
+    	System.out.println(Orderlist);
+    	List<DetailedOrder> DetailedOrderlist = new ArrayList<DetailedOrder>();
+    	
+    	Trip tmp = new Trip();
+    	tmp.title = "《玩美加族》加勒比海展望號遊輪牙買加11日";
+    	tmp.travelCode = 43;
+    	tmp.price = 100;
+    	tmp.startDate = "2020-09-04";
+    	tmp.endDate = "2020-09-13";
+    	
+    	
+		
+		for(Order order: Orderlist) {
+		//for(int i = 0; i < Orderlist.size(); ++i) {
+			DetailedOrder detailedOrder = new DetailedOrder();
+			detailedOrder.id = order.id;
+			detailedOrder.quantity = order.quantity;
+			detailedOrder.trip = tmp;
+			detailedOrder.totalPrice = tmp.price * order.quantity;
+			DetailedOrderlist.add(detailedOrder);
+		}
 
-    	return Orderlist;
+    	return DetailedOrderlist;
     }
-    
 	
 	@RequestMapping(value="/editOrder", method=RequestMethod.POST)
     public ResponseEntity<?> getSearchResultViaAjax(
-    		@Valid @RequestBody @RequestParam(required = true) int id, @Valid @RequestBody @RequestParam(required = false) String quantity, 
-    		@Valid @RequestBody @RequestParam(required = false) String dateIn, @Valid @RequestBody @RequestParam(required = false) String dateOut,
+    		@Valid @RequestBody @RequestParam(required = true)  String id,
+    		@Valid @RequestBody @RequestParam(required = false) String quantity,
     		@Valid @RequestBody @RequestParam(required = false) String action) {
 		System.out.println("editOrder");
+        
+        System.out.println(id + " " + quantity + " " + action);
+        List<Order> Orderlist = Global.db.getOrder(loginInfo.account.id);
+        Order modifyOrder = null;
+        for(Order order: Orderlist) {
+        	System.out.println(order);
+        	if(order.id.equals(id)) {
+        		modifyOrder = order;
+        		break;
+        	}
+        }
+        boolean status = false;
         AjaxResponseBody result = new AjaxResponseBody();
-        System.out.println(id + " " + quantity + " " + dateIn + " " + dateOut + " " + action);
-        Order modifyOrder = Global.db.getOrder(id);
-        boolean execute = false;
+        if(modifyOrder != null) {
+            if(action.equals("delete"))
+            	status = deleteCustomerOrder(id);
+            else if(action.equals("edit"))
+            	status = modifyCustomerOrder(modifyOrder, quantity);
+        }
+        else
+        	errorMessage = "Can not find corresponding order.";
         
-        if(action.equals("delete")) {
-        	execute = deleteCustomerOrder(modifyOrder);
-        }
-        else if(action.equals("edit")) {
-        	String message = reasonable(dateIn, dateOut, quantity);
-        	if(message == null) {
-        		execute = modifyCustomerOrder(modifyOrder, quantity, dateIn, dateOut);
-        	}
-        	else {
-        		result.setMsg(message);
-        	}
-        }
-        
-        if(execute == true) {
-        	result.setMsg("edit successfully!");
-        }
-        else{
-        	result.setMsg("edit failed!");
-        }
-        
-
+        if(status) 
+        	result.setMsg("Edit successfully!");
+        else
+        	result.setMsg(errorMessage);
+        	
         //If error, just return a 400 bad request, along with the error message
         //return ResponseEntity.badRequest().body(result);
         return ResponseEntity.ok(result);
 	} 
 
-    public boolean deleteCustomerOrder(Order deleteOrder){
-    	return Global.db.cancelOrder(deleteOrder);
+    public boolean deleteCustomerOrder(String deleteOrderID){
+    	return Global.db.cancelOrder(loginInfo.account.id, deleteOrderID);
     }
     
-    public boolean modifyCustomerOrder(Order modifyOrder,String Quantity, String datein, String dateout) {   //modify -> re getCustomer
-    	if(Quantity != null) {
-    		modifyOrder.quantity = Integer.parseInt(Quantity);
-    		System.out.println("change quantity");
+    public boolean modifyCustomerOrder(Order modifyOrder, String Quantity) {   //modify -> re getCustomer
+    	
+		System.out.println(Quantity);
+		int inputQuantity;
+		try {
+		inputQuantity = Integer.parseInt(Quantity);
+		}
+		catch(Exception e) {
+    		errorMessage = "Can not parse input quantity to int";
+    		return false;
     	}
-    	if(datein != null) {
-    		modifyOrder.dateIn = datein;
-    		System.out.println("change datein");
-    	}
-    	if(dateout != null) {
-    		modifyOrder.dateOut = dateout;
-    		System.out.println("change dateout");
-    	}
-    	else {
-    		System.out.println("change failed");
-    	}
-    	System.out.println(modifyOrder);
-    	return Global.db.modifyOrder(modifyOrder);
+		System.out.println("parse success");
+		
+		Trip tmp = new Trip();
+		tmp.lowerBound = 10;
+    	tmp.upperBound = 30;
+    	System.out.println(tmp.lowerBound + " " + tmp.upperBound + " " + inputQuantity);
+    	System.out.println(loginInfo.account.id);
+    	System.out.println(modifyOrder.id);
+		
+		if(tmp.lowerBound + inputQuantity <= tmp.upperBound) // check inputQuantity range
+			return Global.db.modifyOrder(loginInfo.account.id, modifyOrder.id, inputQuantity);
+		else {
+			errorMessage = "Quantity range illegal. Legal quantity range: 0 ~ " + (tmp.upperBound - tmp.lowerBound);
+			System.out.println(errorMessage);
+			return false;
+		} 	
     }
-    
-    public static String reasonable(String datein, String dateout, String Quantity) {
-    	String message = null;
-    	int quantity = 0;
-    	if(Quantity != null) {
-    		quantity = Integer.valueOf(Quantity);
-    	}
-    	int dateinYear = Integer.valueOf(datein.substring(0, 3));
-    	int dateinMonth = Integer.valueOf(datein.substring(5, 6));;
-    	int dateinDay = Integer.valueOf(datein.substring(8, 9));;
-    	int dateoutYear = Integer.valueOf(datein.substring(0, 3));;
-    	int dateoutMonth = Integer.valueOf(datein.substring(5, 6));;
-    	int dateoutDay = Integer.valueOf(datein.substring(8, 9));;
-    	boolean oddinMonth = true;
-    	boolean oddoutMonth = true;
-    	
-    	if(dateinMonth == 2 || dateinMonth == 4 || dateinMonth == 6 || dateinMonth == 8 || dateinMonth == 10 || dateinMonth == 12) {
-    		oddinMonth = false;
-    	}
-    	if(dateoutMonth == 2 || dateoutMonth == 4 || dateoutMonth == 6 || dateoutMonth == 8 || dateoutMonth == 10 || dateoutMonth == 12) {
-    		oddoutMonth = false;
-    	}
-    	
-	  try{
-		     java.text.SimpleDateFormat dFormat = new SimpleDateFormat("yyyy/MM/dd");  
-		     dFormat.setLenient(false);
-
-		     java.util.Date d = dFormat.parse(datein);
-		     System.out.println("datein ok");
-		   }catch(ParseException e){
-		    message = "please enter correct date";
-		   }
-	  
-	  try{
-		     java.text.SimpleDateFormat dFormat = new SimpleDateFormat("yyyy/MM/dd");  
-		     dFormat.setLenient(false);
-		    
-		     java.util.Date d = dFormat.parse(dateout);
-		     System.out.println("dateout ok");
-		   }catch(ParseException e){
-		    message = "please enter correct date";
-	   
-		   }
-    	
-    	
-    	if(datein.length() > 10 || dateout.length() > 10) {
-    		message = "please enter correct date!";
-    	}
-    	else if(datein.substring(4, 5).equals("/") == false || datein.substring(7, 8).equals("/") == false) {
-    		message = "please enter date in correct format!(use /)";
-    		System.out.println(datein + datein.substring(7, 8));
-    	}
-    	else if(quantity <= 0 || Quantity.equals("")) {
-    		message = "please enter correct quantity!";
-    	}
-    	
-    	return message;
-    }
-    */
-
 }
 
 
